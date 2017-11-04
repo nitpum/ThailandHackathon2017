5e26bb91b7f36a6607d21cd1c95afb4f93287d62
yes2023 <33365762+yes2023@users.noreply.github.com>
2017-11-04 13:25:52 +0700

    Add files via upload

---
+++ 4yong.c
@@ -0,0 +1,26 @@
+if(data[i].caller >= 0810000000 && data[i].caller < 0819999999){
+		if(data[i].promo == 811){
+
+			}
+		else if(data[i].promo == 812){
+
+			}
+		else if(data[i].promo == 813){
+
+			}
+		}
+if(data[i].caller >= 0820000000 && data[i].caller < 0829999999){
+		if(data[i].promo == 821){
+		
+			}
+		else if (data[i].promo == 822){
+
+			}
+		}
+if(data[i].caller >= 0830000000 && data[i].caller < 0839999999){
+		if(data[i].promo == 831){
+		
+			}
+		else if(data[i].promo == 832){
+
+			}
